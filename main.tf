9137162f2a8b999a24080560b8bee2007d73c677
amartech999 <amartech999@gmail.com>
2025-10-24 18:47:29 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,31 @@
+# Data source to fetch AWS region
+data "aws_region" "current" {}
+
+# Data source to get latest Amazon Linux AMI
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+# Create an S3 bucket
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = var.bucket_name
+  tags = {
+    Environment = "Dev"
+  }
+}
+
+# Create an EC2 instance using the AMI data source
+resource "aws_instance" "demo_ec2" {
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformDemoInstance"
+  }
+}
